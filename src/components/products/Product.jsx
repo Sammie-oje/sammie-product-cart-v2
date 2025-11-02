fd3115bac0dbe87010886b2a8672ebe6f3fe06ad
Sammie-oje <omokhojesamuel8@gmail.com>
2025-11-02 17:29:42 +0100

    Set conditional rendering to the cart button with a state variable and used the picture element to render different images depending on  screen size

--- src/components/products/Product.jsx
+++ src/components/products/Product.jsx
@@ -1,41 +1,98 @@
 import data from "../../data.json";
+import "../../index.css";
+import { useState } from "react";
+
+function ProductCard({ item }) {
+    const [count, setCount] = useState(0);
+
+    return (
+        <article key={item.category} className="mb-6">
+            <div
+                className={`relative ${
+                    count > 0 ? "border-2 border-red rounded-lg" : ""
+                }`}
+            >
+                <figure>
+                    {/*Show appropriate image based on device width*/}
+                    <picture>
+                        <source
+                            srcSet={item.image.mobile}
+                            media="(min-width: 320px)"
+                        />
+                        <source
+                            srcSet={item.image.tablet}
+                            media="(min-width: 768px)"
+                        />
+                        <source
+                            srcSet={item.image.desktop}
+                            media="(min-width: 1280px)"
+                        />
+                        <img
+                            src={item.image.mobile}
+                            alt={item.category}
+                            className="rounded-lg w-full"
+                        />
+                    </picture>
+                </figure>
+                {/*Render two separate elements depending on count */}
+                {count === 0 ? (
+                    <div
+                        className="absolute-el bg-white flex items-center justify-center gap-2"
+                        onClick={() => setCount(1)}
+                    >
+                        <img
+                            src="../../../assets/images/icon-add-to-cart.svg"
+                            alt="Add-to-cart icon"
+                        />
+                        <span className="text-sm font-semibold">
+                            Add to Cart
+                        </span>
+                    </div>
+                ) : (
+                    <div className="absolute-el bg-red border-none text-white flex justify-evenly items-center">
+                        <button className="border border-white rounded-full size-6 flex items-center justify-center" onClick={() => setCount(count - 1)}>
+                            <img
+                                src="../../../assets/images/icon-decrement-quantity.svg"
+                                alt="minus icon"
+                            />
+                        </button>
+                        <span>{count}</span>
+                        <button className="border border-white rounded-full size-6 flex items-center justify-center" onClick={() => setCount(count + 1)}>
+                            <img
+                                src="../../../assets/images/icon-increment-quantity.svg"
+                                alt="plus icon"
+                            />
+                        </button>
+                    </div>
+                )}
+            </div>
+
+            <div className="mt-7">
+                <p
+                    className="text-sm text-red-500 font-light mb-1
+      "
+                >
+                    {item.category}
+                </p>
+                <h3 className="text-base text-red-900 font-semibold">
+                    {item.name}
+                </h3>
+                <span className="text-base text-red font-semibold">
+                    ${item.price.toFixed(2)}
+                </span>
+            </div>
+        </article>
+    );
+}
 
 function Product() {
-  const product = data.map((item) => (
-    <article key={item.category} className="mb-6">
-      <div className="relative">
-        <figure>
-          <img
-            src={`${item.image.mobile}`}
-            alt=""
-            className="w-full rounded-lg"
-          />
-        </figure>
-        <button className="absolute left-1/2 bottom-0 translate-x-[-50%] translate-y-1/2 flex items-center justify-center gap-2 border border-red-400 hover:border-red hover:text-red bg-white w-40 h-[2.75em] rounded-full cursor-pointer ">
-          <img
-            src="../../../assets/images/icon-add-to-cart.svg"
-            alt="Add-to-cart icon"
-          />
-          <span className="text-sm font-semibold">Add to Cart</span>
-        </button>
-      </div>
-
-      <div className="mt-7">
-        <p
-          className="text-sm text-red-500 font-light mb-1
-        "
-        >
-          {item.category}
-        </p>
-        <h3 className="text-base text-red-900 font-semibold">{item.name}</h3>
-        <span className="text-base text-red font-semibold">
-          ${item.price.toFixed(2)}
-        </span>
-      </div>
-    </article>
-  ));
-
-  return <>{product}</>;
+    return (
+        <>
+            {data.map(item => (
+                <ProductCard item={item} />
+            ))}
+        </>
+    );
 }
 
 export default Product;
